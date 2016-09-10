cd789f2ade7c6e3fdc9208778c536d70ca3941fd
balwinder789 <200326404@student.georgianc.on.ca>
2016-09-09 21:52:43 -0400

    after changing

---
+++ Lab1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_F2016_Lab1
+{
+    public partial class Lab1 : Form
+    {
+        public Lab1()
+        {
+            InitializeComponent();
+        }
+
+       
+      
+        private void ClickMeButton_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("HIII");
+        }
+        private void ExitButton_Click(object sender, EventArgs e)
+         { 
+             Application.Exit(); 
+      }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello woRLD!");
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            MessageBox.Show("good bye!");
+        }
+    }
+}
